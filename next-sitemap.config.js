df112a93f8fa2f94662535228a1d8893af3ff0a2
Ranith04 <136628558+Ranith04@users.noreply.github.com>
2025-09-11 12:20:38 +0530

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,25 @@
 module.exports = {
   siteUrl: 'https://globalvisionuae.com',
   generateRobotsTxt: true,
+  exclude: ['/api/*'],
+  generateIndexSitemap: false,
+  changefreq: 'weekly',
+  priority: 0.7,
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    }
+  },
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
 }
